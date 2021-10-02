a4b1ad5f05164b7366330386fad8f62269ffcce8
Michelix28MK <54115538+Michelix28MK@users.noreply.github.com>
2021-10-02 19:13:05 +0200

    Formazione testi alfa per 3 stili

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,6 @@
 ﻿class stile {
-    constructor(name, src, descrizione, componenti) {
+    constructor(name, src, descrizione, componenti) {  //src=link immagine
+    
         this.name = name;
         this.src = src;
         this.descrizione = descrizione;
@@ -7,23 +8,53 @@
     }
 }
 
-let Alt = new stile("Alt", "immagini/alt.jpg", "L'alt (da alternativo - 'alternativ') è uno aestetic che mostra un abbigliamento sui toni scuri o del nero che è reso particolare dai dettagli alternativi quali calze, sclada muscoli, catenelle, (etc.) e dalla sovrapposizione di diversi stili.",
+
+
+
+
+
+let Alt = new stile("Alt", "immagini/alt.jpg", "L'alt (da alternativo) è uno aesthetic che mostra un abbigliamento sui toni scuri o del nero che è reso particolare dai dettagli alternativi quali calze, sclada muscoli, catenelle, (etc.) e dalla sovrapposizione di diversi stili.",
     ["Calze a rete", "Maglia a maniche lunghe", "gonna corta"]);
+
 let Artsy = new stile("Artsy","immagini/Artsy.jpg", "L'Artsy è un aesthetic che riprende temi artistici come maglie e jeans decorati a mano con pittura o ricami, uno stile semplice e leggero.",
-    ["jeans", "Boh"]);
-let Classy = new stile("Classy", "immagini/classy.jpg", "Il Classy è un aestetic che evidenzia uno stile molto chic ed elegante, caratterizzato da vestiti da sera e gioielli.",
-    ["a", "b"]);
+    ["Mom jeans", "Vestiti personalizzati: ispirato alla natura", "Vestiti multicolore", "Scarpe Vans, Converse o vintage"]); //too check
+
+let Boujee = new stile("Boujee", "immagini/classy.jpg", "Il Boujee è un aesthetic molto stravagante, chic, elegante e stiloso. É associato ad uno stile di vita altolocato, a comprare e indossare i vestiti o accessori degli ultimi trend.É caratterizzato da vestiti brillanti e da sera e gioielli sfarzosi.",
+    ["Abiti da sera", "Abiti in velluto","Abiti brillanti", "Abiti lunghi con spacchi", "Orecchini lunghi o diamantati", "Tacchi medio-alti"]);
+
 let CottageCore = new stile("CottageCore", "immagini/Cottagecore.jpg","Il Cottagecore è uno aestetic fashion che celebra e ricorda la vita rurale. Valorizza le capacità tradizionali e la nostalgica vita di campagna.",
     ["a", "b"]);
+
 let DarkAcademia = new stile("DarkAcademia", "immagini/dark_Academia.jpg", "Il dark academia è uno aestetic che rappresenta uno stile accademico e scolastico con dei temi scuri ed autunnali come il nero ed il marrone.",
     ["a", "b"]);
+
 let LightAcademia = new stile("LightAcademia", "immagini/light_academia.jpg", "Il dark academia è uno aestetic che rappresenta uno stile accademico e scolastico con dei temi chiari ed primaverili come il bianco o 'color panna' ed il marroncino.",
     ["a", "b"]);
+
 let Indie = new stile("Indie", "immagini/indie.jpg", "L'alt (da alternativo - 'alternativ') è uno aestetic che mostra un abbigliamento sui toni scuri o del nero che è reso particolare dai dettagli alternativi quali calze, sclada muscoli, catenelle, (etc.) e dalla sovrapposizione di diversi stili.",
     ["a", "b"]);
-let Skater = new stile("Skater", "immagini/skater.jpg", " ", " ");
 
-let listaStili = [Alt, Artsy, Classy, CottageCore, DarkAcademia, LightAcademia, Indie, Skater];
+let Skater = new stile("Skater", "immagini/skater.jpg", "skater, conosciuto anche come skatercore, è un aesthetic centrato sulla cultura dello skate, dove le persone indossano vestiti larghi e comodi con grafiche vintage o di musica elettronica, pop e hip-hop.",["Marchio Vans", "Baggy jeans", "Magliette larghe", "Scarpe a suola piatta", "Cappello da pescatore o da baseball"]); //too check
+
+let Y2K = new stile("Y2K", "immagini/Y2K.jpeg", "Y2K è un aesthetich che era prevalente nella cultura popolare dagli anni dal 1995 al 2004,è una moda ritornata ed è caratterizzata da un distinto periodo stilistico, hardware design, molta musica hip-hop e pop, accessori luccicanti e il riutuilizzo delle vechie 'tecnologie'", ["minigonne: principalmente fatte in jeans si possono trovare anche in altri materiali.","Top: colorati e stravaganti con grafiche vintage.","Gioelli luccicanti","tute: colorate in velluto"]); //too check
+
+
+
+
+
+let listaStili = [Y2K, Alt, Artsy, Boujee, CottageCore, DarkAcademia, LightAcademia, Indie, Skater];
+
+
+
+
+
+
+
+
+
+
+
+
 
 function creaSezioni(stile, _posizione) {
 
